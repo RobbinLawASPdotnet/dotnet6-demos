2850e452fc017a7f2e10bf24b9d3fbcad58b522f
robbinl <robbinl@telusplanet.net>
2022-08-26 12:41:29 -0600

    initial copy from dotnetcore5 stuff

---
+++ aa-oop-review/bb-try-catch-finally/Program.cs
@@ -0,0 +1,71 @@
+﻿//https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/exceptions/exception-handling
+using System;
+
+namespace try_catch_finally
+{
+    class Person
+    {
+        public string Name;
+
+        public Person(string name)
+        {
+            if(name == "robbin")
+                throw new ArgumentNullException($"Bad Name: {name}");
+            if(name == "robbinl")
+                throw new FormatException($"Bad Name: {name}");
+            if(name == "robbinlaw")
+                throw new FieldAccessException($"Bad Name: {name}");
+
+            Name = name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main method started");
+            var app = new Program();
+            var temp = app.Ex01();
+            Console.WriteLine($"Ex01() returned string: {temp}");
+            Console.WriteLine("Main method ended");
+        }
+
+        private string Ex01()
+        {
+            try
+            {
+                Console.WriteLine("Ex01 try started");
+                Person personreference = new Person("rob");
+                Console.WriteLine($"My name is: {personreference.Name}");
+                Console.WriteLine("Ex01 try ended");
+                //return "try";
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"ArgumentNullException in Ex01: {ex.Message}");
+                return "ArgumentNullException";
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"FormatException in Ex01: {ex.Message}");
+                return "FormatException";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CatchAllException in Ex01: {ex.Message}");
+                //throw;
+                //return "CatchAllException";
+            }
+            finally
+            {
+                Console.WriteLine("The finally runs exception or not");
+                //can only have one finally per try
+                //cannot have a return in finally
+                //return "finally";
+            }
+            Console.WriteLine("This code will run if the catch does not have a return");
+            Console.WriteLine("This code will also run if the try is successful");
+            return "funny thing";  
+        }
+    }
+}
---
+++ bb-console-app-westwind/Supplier.cs
@@ -0,0 +1,25 @@
+using System;
+namespace WestWindSystem
+{
+    public class Supplier
+    {
+        public string CompanyName {get; set;}
+        public string PhoneNumber {get; set;}
+
+        public Supplier(string companyName, string phoneNumber)
+        {
+            if(string.IsNullOrWhiteSpace(companyName))
+                throw new ArgumentNullException("companyName cannot be null or empty");
+            if(string.IsNullOrWhiteSpace(phoneNumber))
+                throw new ArgumentNullException("phoneNumber cannot be null or empty");
+                
+            CompanyName = companyName.Trim();
+            PhoneNumber = phoneNumber.Trim();
+        }
+
+        public override string ToString()
+        {
+            return $"Company Name: {CompanyName}, Phone Number: {PhoneNumber}";
+        }
+    }
+}
